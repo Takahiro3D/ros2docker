5ba17a79e8ce4a7fb455bbc34ec88c2f68053f95
dorebom <dorebom.b@gmail.com>
2021-04-09 14:05:15 +0900

    Mod: xxx

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ WORKSPACE=workspace/github/ros2docker/ros2_ws
 
 docker run --rm -it --privileged \
         --gpus all \
+        --device=/dev/ttyUSB0:/dev/ttyUSB0 \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v /etc/localtime:/etc/localtime \
         -v /home/$USER_NAME/$WORKSPACE:/home/developer/ros2_ws \
